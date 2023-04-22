d22dede5b7520a1d7041cfff9fd44832a6db6f98
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-04-22 14:24:50 +0300

    finish 3.15 and 3.16 puhelinluettelo ja tietokanta steps 3 and 4

--- index.js
+++ index.js
@@ -26,16 +26,25 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${Person.length} people</p><p>${Date()}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(persons => {
-        response.json(persons)
-    }) 
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        }) 
+        .catch(error => next(error))
 })
 
-/*
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-*/
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -55,6 +64,24 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })    
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
