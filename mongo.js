5b2be0cabf70e74066ace36c3c0f807ca54cfc32
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-04-22 11:08:35 +0300

    finish 3.12 tietokanta komentoriviltä

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const pw = process.argv[2]
+
+const url = 
+    `mongodb+srv://miskapaju:${pw}@puhelinluettelo.nnitj8e.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Contact.find({}).then(result => {
+        result.forEach(contact => {
+            console.log(contact.name, contact.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 4) {
+    console.log('incorrect amount of parameters')
+    mongoose.connection.close()
+}
+
+if (process.argv.length === 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    
+    const contact = new Contact({
+        name: name,
+        number: number,
+    })
+    
+    contact.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
