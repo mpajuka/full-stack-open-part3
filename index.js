0470d64a7e8d3ae050a8741cac6f21c276991d8a
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-04-22 18:24:05 +0300

    finish 3.19 puhelinluettelo ja tietokanta, step7

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -60,9 +60,12 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })    
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
+        
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -91,6 +94,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
