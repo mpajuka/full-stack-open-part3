e6ab533cabbe4e1781f4411244aced0fcdd4438c
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-04-22 18:45:37 +0300

    finish 3.20 puhelinluettelo ja tietokanta, step8

--- models/person.js
+++ models/person.js
@@ -17,8 +17,19 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         minlength: 3,
+        required: [true, 'Name cannot be empty']
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,}$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: [true, 'Persons phone number is required']
+    }, 
 })
 
 personSchema.set('toJSON', {
